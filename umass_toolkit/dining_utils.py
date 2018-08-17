b45fa21701a611d5ac13caeb1c26634f89962494
mattrossman <mrossman@umass.edu>
2018-08-17 15:09:08 -0400

    Flatten menu response and refactor menu functions Moved the HTML parsing function into the utility module since users won't be calling it. When an attribute has an empty value (e.g. '') the returned dish will hold the attribute with a `None` value instead of ignoring the attribute. Resolves #19

--- umass_toolkit/dining_utils.py
+++ umass_toolkit/dining_utils.py
@@ -1,3 +1,10 @@
+from bs4 import BeautifulSoup
+import pint
+
+
+ureg = pint.UnitRegistry()
+
+
 #TODO: handle multiple levels of parentheses
 #e.g. Chili (spicy (but not too spicy))
 def parse_list(ingredients):
@@ -24,3 +31,29 @@ def parse_list(ingredients):
         ingredient_list.append(ingredient)
         i += 1
     return ingredient_list
+
+def category_html_to_dict(html_string, meal, category):
+    soup = BeautifulSoup(html_string, 'html.parser')
+    items = soup.find_all('a', href='#inline')
+    ret = []
+    for item in items:
+        dish = {}
+        dish['category-name'] = category
+        dish['meal-name'] = meal
+        for attribute in item.attrs.keys():
+            if attribute.startswith('data-') and not attribute.endswith('dv'):
+                attribute_name = attribute[5:]
+                data = item.attrs[attribute]
+                if attribute_name == 'calories' or attribute_name == 'calories-from-fat':
+                    data = int(data) if data else None
+                elif attribute_name == 'clean-diet-str':
+                    data = data.split(', ')
+                    attribute_name = 'diets'
+                elif attribute_name in ['allergens', 'ingredient-list']:
+                    data = parse_list(data)
+                elif attribute_name in ['cholesterol', 'sodium', 'dietary-fiber', 'protein', 'sat-fat', 'sugars',
+                                        'total-carb', 'total-fat', 'trans-fat']:
+                    data = ureg.Quantity(data) if data else None
+                dish[attribute_name] = data
+        ret.append(dish)
+    return ret
